cb4c99a0a7d1572d4c6de2e99ada6dfe8983f5ad
Karan Singh <119948976+thekaran69@users.noreply.github.com>
2024-04-30 15:21:40 +0530

    Updated

--- app.js
+++ app.js
@@ -1,23 +1,68 @@
 let gameContainer =  document.querySelector(".game-container");
-
+let scoreContainer = document.querySelector(".score-container");
 
 
 let foodX,foodY;
 let headX = 12,headY = 12;
 let velocityX=0,velocityY=0;
+let snakeBody = [];
+let score = 0;
 
 function generateFood(){
     foodX = Math.floor(Math.random()*25 +1);
     foodY = Math.floor(Math.random()*25 +1);
+    for(let i=0;i<snakeBody.length;i++){
+        if(snakeBody[i][1] == foodY && snakeBody[i][0] == foodX){
+            generateFood();
+        }
+    }
+
+}
+
+function gameOver(){
+    headX = 12;
+    headY = 12;
+    generateFood();
+    velocityX = 0;
+    velocityY = 0;
+    snakeBody = [];
+    score = 0;
+    scoreContainer.innerHTML = "Score : " + score
+    alert("Game Over");
 }
 
 
-
 function renderGame(){
-    let updatedGame = `<div class="food" style="grid-area: ${foodY}/${foodX};"></div>  `;
+    console.log("Rendered ");
+    let updatedGame = `<div class="food" style="grid-area: ${foodY}/${foodX};"></div>`;
+    if(foodX == headX && headY == foodY){
+        snakeBody.push([foodX,foodY]);
+        generateFood();
+        score+=10;
+        scoreContainer.innerHTML = "Score : " + score
+    }
 
+
+    snakeBody.pop();
     headX+=velocityX;
     headY+=velocityY;
+    snakeBody.unshift([headX,headY]);
+    if(headX == 0 || headY == 0 || headX == 26 || headY == 26){
+        gameOver();
+    }
+
+
+    for(let i=1;i<snakeBody.length;i++){
+        if(snakeBody[0][0] == snakeBody[i][0] && snakeBody[0][1] == snakeBody[i][1]){
+            gameOver();
+        }
+
+    }
+
+    for(let i=0;i<snakeBody.length;i++){
+        updatedGame += `<div class="snake" style="grid-area: ${snakeBody[i][1]}/${snakeBody[i][0]};"></div>`
+    }
+    
 
     updatedGame += `<div class="snake" style="grid-area: ${headY}/${headX};"></div>  `;
 
@@ -33,19 +78,19 @@ document.addEventListener("keydown",function(e){
 
     let key = e.key;
 
-    if(key == "ArrowUp"){
+    if(key == "ArrowUp"  && velocityY!=1){
         velocityX = 0;
         velocityY = -1;
     }
-    else if(key == "ArrowDown"){
+    else if(key == "ArrowDown"  && velocityY!=-1){
         velocityX = 0;
         velocityY = 1;
     }
-    else if(key == "ArrowLeft"){
+    else if(key == "ArrowLeft"  && velocityX!=1){
         velocityY = 0;
         velocityX = -1;
     }
-    else if(key == "ArrowRight" ){
+    else if(key == "ArrowRight"  && velocityX!=-1){
         velocityY = 0;
         velocityX = 1;
     }
